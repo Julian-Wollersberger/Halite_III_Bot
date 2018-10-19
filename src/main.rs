403612ba835356274e2ad119f1b98620b287d9a7
Julian Wollersberger <julian.derzocker@gmx.at>
2018-10-19 12:28:28 +0200

    Go around spawn: A bot that constantly travels around spawn.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 extern crate rand;
 
-use hlt::command::Command;
-use hlt::direction::Direction;
 use hlt::game::Game;
-use rand::Rng;
-use rand::SeedableRng;
-use rand::XorShiftRng;
 use std::env;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
 
 mod hlt;
+mod fixed_pattern_bot;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,51 +15,14 @@ fn main() {
     } else {
         SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()
     };
-    let seed_bytes: Vec<u8> = (0..16).map(|x| ((rng_seed >> (x % 8)) & 0xFF) as u8).collect();
-    let mut rng: XorShiftRng = SeedableRng::from_seed([
-        seed_bytes[0], seed_bytes[1], seed_bytes[2], seed_bytes[3],
-        seed_bytes[4], seed_bytes[5], seed_bytes[6], seed_bytes[7],
-        seed_bytes[8], seed_bytes[9], seed_bytes[10], seed_bytes[11],
-        seed_bytes[12], seed_bytes[13], seed_bytes[14], seed_bytes[15]
-    ]);
 
-    let mut game = Game::new();
+    let game = Game::new();
     // At this point "game" variable is populated with initial map data.
     // This is a good place to do computationally expensive start-up pre-processing.
     // As soon as you call "ready" function below, the 2 second per turn timer will start.
-    Game::ready("MyRustBot");
+    Game::ready("Julius-Beides");
 
     game.log.borrow_mut().log(&format!("Successfully created bot! My Player ID is {}. Bot rng seed is {}.", game.my_id.0, rng_seed));
 
-    let mut initial_turn = true;
-
-    loop {
-        game.update_frame();
-        let mut command_queue: Vec<Command> = Vec::new();
-        let me = &game.players[game.my_id.0];
-
-        // Initial turn spawns a ship.
-        if initial_turn {
-            command_queue.push(me.shipyard.spawn());
-            initial_turn = false;
-        }
-
-
-        // move up
-        for ship_id in &me.ship_ids {
-            let ship = &game.ships[ship_id];
-
-            /*let command = if cell.halite < game.constants.max_halite / 10 || ship.is_full() {
-                let random_direction = Direction::get_all_cardinals()[rng.gen_range(0, 4)];
-                ship.move_ship(random_direction)
-            } else {
-                ship.stay_still()
-            };*/
-
-            let command = ship.move_ship(Direction::North);
-            command_queue.push(command);
-        }
-
-        Game::end_turn(&command_queue);
-    }
+    fixed_pattern_bot::run(game);
 }
